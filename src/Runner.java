1fd3b8d1ebb9ecee9a3dae8662005b147fdf12fd
BT_1N3_30 <d>
2019-05-20 10:10:37 -0400

    fw

--- src/Runner.java
+++ src/Runner.java
@@ -10,7 +10,22 @@ public class Runner
         {
             System.out.println(abc);
         }
-        
+        boolean kok = a.isBalanced(bc);
+        System.out.println(kok);
+
+        StepTracker tr = new StepTracker(10000);
+        System.out.println(tr.activeDays());
+        System.out.println(tr.averageSteps());
+        tr.addDailySteps(9000);
+        tr.addDailySteps(5000);
+        System.out.println(tr.activeDays()) ;
+        tr.addDailySteps(13000);
+        System.out.println(tr.activeDays());
+        System.out.println(tr.averageSteps());
+        tr.addDailySteps(23000);
+        tr.addDailySteps(1111);
+        System.out.println(tr.activeDays());
+        System.out.println(tr.averageSteps());
 
     }
 }
