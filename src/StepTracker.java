ab9ed03c8b0e531df961cfe273f2af05711fb14e
BT_1N3_30 <d>
2019-05-20 09:35:37 -0400

    fw

--- src/StepTracker.java
+++ src/StepTracker.java
@@ -1,4 +1,39 @@
+import java.util.ArrayList;
+
 public class StepTracker
 {
-    
+    private int minSteps;
+    private int activeDays;
+    private ArrayList<Integer> days;
+
+    public StepTracker(int steps)
+    {
+        minSteps = steps;
+        activeDays = 0;
+        days = new ArrayList<>();
+    }
+
+    public void addDailySteps(int steps)
+    {
+        days.add(steps);
+        if(steps >= minSteps)
+        {
+            activeDays++;
+        }
+    }
+
+    public int activeDays()
+    {
+        return activeDays;
+    }
+
+    public double averageSteps()
+    {
+        double count = 0;
+        for(int i = 0; i < days.size(); i++)
+        {
+            count += days.get(i);
+        }
+        return count/days.size();
+    }
 }
